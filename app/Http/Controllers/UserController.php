2bffda360e857a9396da3a1f530ddb9c36701606
EdsonRosas3 <edsonrosas321@gmail.com>
2021-05-16 23:44:35 -0400

    asdfaf

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
             return response()->json(['success' => $success], $this-> successStatus);
         } 
         else{ 
-            return response()->json(['error'=>'Unauthorised'], 401); 
+            return response()->json(['error'=>'Datos incorrectos. Por favor revise su nombre de usuario y contraseña'], 401); 
         } 
     }
 
