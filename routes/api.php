e7332e789a432a01be02bfb7abc95f560fafd9b4
EdsonRosas3 <edsonrosas321@gmail.com>
2021-06-21 01:42:20 -0400

    asdasf

--- routes/api.php
+++ routes/api.php
@@ -49,9 +49,10 @@ Route::get('files/{id}', 'RequestQuotitationController@showFiles');
 Route::get('requestquotitationpdf/{id}','PDFQuotitationController@requestquotitationPDF');
 
 /*ARCHIVOS DE COTIZACION */
-/**Devuelve el file por ID*/
+/**nombre de archivo del detalle de una cotizacion */
 Route::get('quotation/files/detail/{idDetailOffert}', 'QuoteResponseController@showNameFilesDetailsBusiness');
-
+/**muestra el archivo del detalle de una cotizacion*/ //no tiene un servicio en frontend
+Route::get("quotation/showFiles/detail/{namefile}", "QuoteResponseController@showFilesDetailsBusiness");
 
 /**Dentro de este grupo de rutas solo podran acceder si han iniciado sesion por lo tanto tiene que 
  * pasar el token para poder usar las rutas dentro del grupo
