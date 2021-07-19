5e745a81b2b0c2f86c24a6ade66062ef124447b7
valeria2204 <valeria.caritasm@gmail.com>
2021-07-18 23:33:40 -0400

    add

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\AdministrativeUnit;
+use App\RequestDetail;
+use App\RequestQuotitation;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(FacultySeeder::class);
+        $this->call(AdministrativeUnitSeeder::class);
+        $this->call(SpendingUnitSeeder::class);
+        $this->call(PermissionSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(LimiteAmountSeeder::class);
+        $this->call(RequestQuotationSeeder::class);
+        $this->call(RequestDetailSeeder::class);
+        $this->call(ReportSeeder::class);
+        $this->call(BusinessSeeder::class);
     }
 }
